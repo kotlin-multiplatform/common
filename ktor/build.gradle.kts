3c8daf59286e1a79345f1583089b832c5869c716
Sebastian Owodzin <sebastian.owodzin@gmail.com>
2024-02-27 22:19:26 +0000

    setup github workflows

--- ktor/build.gradle.kts
+++ ktor/build.gradle.kts
@@ -1,11 +1,9 @@
 import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
-import com.palantir.gradle.gitversion.VersionDetails
-import groovy.lang.Closure
 
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.multiplatform)
-    alias(libs.plugins.palantir.gitVersion)
+//    alias(libs.plugins.palantir.gitVersion)
     id("maven-publish")
 }
 
@@ -73,28 +71,26 @@ android {
     }
 }
 
-fun versionString(): String {
-    val versionDetails: Closure<VersionDetails> by extra
-
-    try {
-        with(versionDetails()) {
-            listOfNotNull(
-                lastTag,
-                commitDistance.takeIf { commitDistance > 0 },
-                gitHash.takeIf { commitDistance > 0 }
-            ).joinToString("-")
-        }
-    } catch (e: Throwable) {
-        "0.1"
-    }.also {
-        println("versionString: $it")
-    }
-
-    return "0.1-SNAPSHOT-1"
-}
+//fun versionString(): String {
+//    val versionDetails: groovy.lang.Closure<com.palantir.gradle.gitversion.VersionDetails> by extra
+//
+//    return try {
+//        with(versionDetails()) {
+//            listOfNotNull(
+//                lastTag,
+//                commitDistance.takeIf { commitDistance > 0 },
+//                gitHash.takeIf { commitDistance > 0 }
+//            ).joinToString("-")
+//        }
+//    } catch (e: Throwable) {
+//        "0.1"
+//    }.also {
+//        println("versionString: $it")
+//    }
+//}
 
 group = "kmp.common"
-version = versionString()
+version = "0.1" //versionString()
 
 private fun username(project: Project) =
     gradleLocalProperties(project.rootDir).getProperty("gpr.user") ?: System.getenv("GITHUB_USER")
